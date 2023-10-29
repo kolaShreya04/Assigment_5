0eefd10d7bb78b3ddeb314ce009600aa4bb45798
kolaShreya04 <142619333+kolaShreya04@users.noreply.github.com>
2023-10-29 22:21:22 +0000

    Edited with Project 3 requirements

---
+++ script.js
@@ -0,0 +1,35 @@
+function validateForm() {
+    // Validation for password matching
+    const password = document.getElementById("password").value;
+    const reenterPassword = document.getElementById("reenterPassword").value;
+
+    if (password !== reenterPassword) {
+        alert("Passwords do not match");
+        return false;
+    }
+
+    // Validation for username availability
+    const existingUsernames = ["Admin", "Harry", "Elizabeth", "Shana"];
+    const username = document.getElementById("username").value;
+
+    if (existingUsernames.includes(username)) {
+        alert("Username is already taken");
+        return false;
+    }
+
+    // Additional validation can be added here
+
+    return true;
+}
+
+var canvas = document.getElementById("goodCanvas1");
+var ctx = canvas.getContext("2d");
+ctx.font= "30px Comic Sans MS";
+ctx.fillStyle = "green";
+ctx.fillRect(0,0,400,100);
+ctx.fillStyle = "white";
+ctx.textAlign = "center";
+ctx.fillText("Hello ARIA World", canvas.width/2, canvas.height/2);
+
+
+document.getElementsByTagName('div')[0].focus();
